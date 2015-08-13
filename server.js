9dbed0937eb5950cfb484bd76a87b02f3ffd5eb0
wangck1998 <wangck1998@foxmail.com>
2015-08-13 10:01:31 +0800

    template

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 'use strict';
 
-var io = require('socket.io')(3000);
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-io.sockets.on('connection', function() {
-  console.log('Client connected...');
-})
+server.listen(3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
