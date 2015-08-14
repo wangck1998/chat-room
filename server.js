bcd4bd06e3181d316920ce47bea0f9180eeb2147
wangck1998 <wangck1998@foxmail.com>
2015-08-14 12:41:00 +0800

    basic functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var path = require('path');
+var EventProxy = require('eventproxy');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -14,9 +15,21 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+io.sockets.on('connection', function (client) {
+  client.on('join', function(name) {
+    console.log(name);
+    client.name = name;
+  });
+  client.on('messages', function(data) {
+
+    var ep = new EventProxy();
+    ep.on('error', function(err) {
+      throw err;
+    });
+
+    io.sockets.emit('messages', {
+      name: client.name,
+      message: data,
+    });
   });
 });
